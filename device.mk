2cc6aefa2324576628636a51132a031586de6039
Nick <nikhilnick00007@gmail.com>
2020-09-20 11:20:40 +0000

    riva: Drop telephony-ext
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- device.mk
+++ device.mk
@@ -376,13 +376,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/riva
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8937
